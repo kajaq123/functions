0864602d9cb85d7ab6c1322f196eb4ed0c076fda
kajkosz <110128846+kajaq123@users.noreply.github.com>
2023-10-16 20:18:54 +0200

    first time
    added system to show the functions on screen in tk, it can display square function and others

---
+++ config.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+window_width = 800
+window_height = 800
+center_x = width = window_width//2
+center_y = height = window_height//2
+
+x1 = -10
+x2 = 10
+dot_r =3
+line_l = 5
+scale = 40
+print("cofig loaded")
+
---
+++ main.py
@@ -0,0 +1,71 @@
+
+y = 2
+
+
+import tkinter as tk
+from numpy import negative, arange
+from config import *
+app = tk.Tk()
+app.title("Function")
+
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+app.geometry(f"{window_width}x{window_height}+{(screen_width-window_width)//2}+{(screen_height-window_height)//2}")
+
+
+canvas = tk.Canvas(app, width=window_width, height=window_height)
+canvas.pack()  
+
+def start():
+    line_x = canvas.create_line(0, window_height//2, window_width, window_height//2, fill="#000000", width=2)
+    line_y = canvas.create_line(window_width//2, 0, window_width//2, window_height, fill="#000000", width=2)
+    for i in range(0, window_width, scale):
+        canvas.create_line(
+            i,
+            center_y - line_l,
+            i ,
+            center_y + line_l,
+            fill="black",
+            width=2
+        )
+
+    for i in range(0, window_height, scale):
+        canvas.create_line(
+            center_x - line_l,
+            i ,
+            center_x + line_l,
+            i ,
+            fill="black",
+            width=2
+        )
+    
+    dot_center = canvas.create_oval(
+    center_x - dot_r,
+    center_y - dot_r,
+    center_x + dot_r,
+    center_y + dot_r,
+    fill="red"  # You can change the fill color as desired
+    )
+
+
+def function1(y):
+    function_list = [] 
+    for x in arange(width*-1, width): # clever system for idk what :)
+        function_list.extend([x+ width, negative((x **y))+ height])  # Add each point as a separate coordinate
+    line = canvas.create_line(*function_list, fill="yellow", width=2)  # Pass the list as arguments
+
+def function2(y):
+    function_list = []  # Create an empty list to store the points
+    for x in arange(width*-1, width,scale): # clever system for idk what :)
+        function_list.extend([x+ width, negative(((x/scale/4) **y))*scale+ height])  # Add each point as a separate coordinate
+    line = canvas.create_line(*function_list, fill="red", width=2)  # Pass the list as arguments
+
+    
+# Draw a line on the screen
+start()
+
+function1(y)
+function2(y)
+
+# Start the Tkinter event loop
+app.mainloop()
